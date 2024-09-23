39d0d6069f695749502476d71a38814610e64cd3
xiaoran <xiaoran2495897824@163.com>
2024-09-23 11:30:18 +0800

    Update: fetch_ids.py

---
+++ fetch_ids.py
@@ -0,0 +1,16 @@
+import subprocess
+import hashlib
+
+
+def fetch_pci_ids():
+    # use subprocess to call bash - wget to get new pci.ids file
+    print("Fetching new PCI IDs...")
+    subprocess.run(["bash", "-c", "wget -q -O ./data/pci.ids.gz https://pci-ids.ucw.cz/v2.2/pci.ids.gz"])
+    subprocess.run(["bash", "-c", "rm ./data/pci.ids"])
+    print("Updating local copy of PCI IDs... ")
+    subprocess.run(["bash", "-c", "gunzip ./data/pci.ids.gz"])
+    print("Finished fetching new PCI IDs.  ")
+
+
+if __name__ == "__main__":
+    fetch_pci_ids()
